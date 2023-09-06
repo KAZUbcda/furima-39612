8de222efa9bf9057a883e392d366bf5c58afe181
KAZUbcda <rstoyo.kazutaka.tomaru@gmail.com>
2023-09-06 18:15:57 +0900

    商品出品ページへのアクセス、ログイン状態によるリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sessions_new, only: [:new]
   # indexアクション定義
   def index
   end
+
+  # newアクション定義
+  def new
+    # @item = Item.new
+  end
+
+  # ここから下privateメソッド
+  private
+  def move_to_sessions_new
+    # ログアウト状態の時に実行される
+    unless user_signed_in?
+      # ユーザーログインページにリダイレクトする
+      redirect_to new_user_session_path
+    end
+  end
+
 end
