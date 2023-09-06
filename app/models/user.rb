667eb19b26ced2ac9a1be2c621596129a2a6d9e6
KAZUbcda <rstoyo.kazutaka.tomaru@gmail.com>
2023-09-06 12:08:48 +0900

    テストコード作成2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
 
   validates :nickname,           presence: true
   validates :email,              presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角(漢字・ひらがな・カタカナ)を使用してください' } do
     validates :first_name
@@ -17,5 +16,5 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :last_name_kana
   end
-  validates :date_of_birth,      presence: true
+  validates :date_of_birth, presence: true
 end
