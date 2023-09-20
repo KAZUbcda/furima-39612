ec2c5d36823e3a675ed549739b58fb1fa911de8b
KAZUbcda <rstoyo.kazutaka.tomaru@gmail.com>
2023-09-20 17:10:24 +0900

    クレジットカード関連機能の実装2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,17 @@
 class OrdersController < ApplicationController
+  before_action :set_item,             only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:item_id])
     @purchase_record_shipping_address = PurchaseRecordShippingAddress.new
   end
 
   def create
     # binding.pry
-    @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:item_id])
     @purchase_record_shipping_address = PurchaseRecordShippingAddress.new(shipping_address_params)
     if @purchase_record_shipping_address.valid?
+      pay_item
       @purchase_record_shipping_address.save
       redirect_to root_path
     else
@@ -20,6 +23,19 @@ class OrdersController < ApplicationController
 
   def shipping_address_params
     params.require(:purchase_record_shipping_address).permit(:post_code, :pref_id, :municipalities, :street_address, :bldg_name, :tel_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_2d5771449a12665767b86f08"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: shipping_address_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
