ab2629b562ee0c2fe89f861791e4fee3f5f00efc
KAZUbcda <rstoyo.kazutaka.tomaru@gmail.com>
2023-10-17 14:01:56 +0900

    エラーメッセージ日本語とuserモデルテストコード修正2とcss修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,13 +7,10 @@ class OrdersController < ApplicationController
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @purchase_record_shipping_address = PurchaseRecordShippingAddress.new
-    @shipping_address = ShippingAddress.new
   end
 
   def create
-    # @purchase_record_shipping_address = PurchaseRecordShippingAddress.new(shipping_address_params)
     @purchase_record_shipping_address = PurchaseRecordShippingAddress.new(purchase_record_shipping_address_params)
-    # @shipping_address = ShippingAddress.new(shipping_address_params)
     if @purchase_record_shipping_address.valid?
       pay_item
       @purchase_record_shipping_address.save
@@ -26,10 +23,6 @@ class OrdersController < ApplicationController
 
   private
 
-  # def shipping_address_params
-  #   params.require(:purchase_record_shipping_address).permit(:post_code, :pref_id, :municipalities, :street_address, :bldg_name, :tel_number)
-  # end
-
   def purchase_record_shipping_address_params
     params.require(:purchase_record_shipping_address).permit(:post_code, :pref_id, :municipalities, :street_address, :bldg_name, :tel_number).merge(
       user_id: current_user.id, item_id: params[:item_id], token: params[:token]
@@ -40,7 +33,7 @@ class OrdersController < ApplicationController
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # 自身のPAY.JPテスト秘密鍵を記述しましょう
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
-      card: shipping_address_params[:token], # カードトークン
+      card: purchase_record_shipping_address_params[:token], # カードトークン
       currency: 'jpy' # 通貨の種類（日本円）
     )
   end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,77 +15,77 @@ RSpec.describe Item, type: :model do
       it 'imageが空では出品できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include "Image can't be blank"
+        expect(@item.errors.full_messages).to include "商品画像を設定してください"
       end
       it 'nameが空では出品できない' do
         @item.name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include "Name can't be blank"
+        expect(@item.errors.full_messages).to include "商品名を入力してください"
       end
       it 'explanationが空では出品できない' do
         @item.explanation = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include "Explanation can't be blank"
+        expect(@item.errors.full_messages).to include "商品の説明を入力してください"
       end
       it 'category_idが1では出品できない' do
         @item.category_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Category can't be blank"
+        expect(@item.errors.full_messages).to include "カテゴリーを設定してください"
       end
       it 'condition_idが1では出品できない' do
         @item.condition_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Condition can't be blank"
+        expect(@item.errors.full_messages).to include "商品の状態を設定してください"
       end
       it 'delivery_charge_idが1では出品できない' do
         @item.delivery_charge_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Delivery charge can't be blank"
+        expect(@item.errors.full_messages).to include "配送料の負担を設定してください"
       end
       it 'pref_idが1では出品できない' do
         @item.pref_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Pref can't be blank"
+        expect(@item.errors.full_messages).to include "発送元の地域を設定してください"
       end
       it 'days_to_ship_idが1では出品できない' do
         @item.days_to_ship_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Days to ship can't be blank"
+        expect(@item.errors.full_messages).to include "発送までの日数を設定してください"
       end
       it 'priceが空では出品できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price can't be blank"
+        expect(@item.errors.full_messages).to include "販売価格を入力してください"
       end
       it 'priceが300未満では出品できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Price は300~9,999,999の範囲で指定してください'
+        expect(@item.errors.full_messages).to include '販売価格は300~9,999,999の範囲で指定してください'
       end
       it 'priceが10,000,000以上では出品できない' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Price は300~9,999,999の範囲で指定してください'
+        expect(@item.errors.full_messages).to include '販売価格は300~9,999,999の範囲で指定してください'
       end
       it 'priceが全角数字では出品できない' do
         @item.price = '３００'
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Price is not a number'
+        expect(@item.errors.full_messages).to include '販売価格は数値で入力してください'
       end
       it 'priceが全角英字では出品できない' do
         @item.price = 'ABCDEF'
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Price is not a number'
+        expect(@item.errors.full_messages).to include '販売価格は数値で入力してください'
       end
       it 'priceが文字では出品できない' do
         @item.price = 'あいう'
         @item.valid?
-        expect(@item.errors.full_messages).to include 'Price is not a number'
+        expect(@item.errors.full_messages).to include '販売価格は数値で入力してください'
       end
       it 'userが紐付いていないと保存できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')
+        expect(@item.errors.full_messages).to include('ユーザーを入力してください')
       end
     end
   end
